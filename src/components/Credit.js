e5a90af8bae8caaa24878a5e4ced655ca22c3ffb
Sonam <sonamyadav291093@gmail.com>
2024-08-19 04:37:09 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a 
+        > Sonam</a
+      >, <a href="https://github.com/shaurya8688/weather-App.git">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
